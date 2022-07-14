de00272287883d35a4928a699cbb689a8b2a4a96
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2022-07-14 15:58:34 +0200

    Add Shiboken

---
+++ pythonforandroid/recipes/shiboken6/__init__.py
@@ -0,0 +1,19 @@
+from pythonforandroid.recipe import PythonRecipe
+from pythonforandroid.logger import info
+import zipfile
+
+class ShibokenRecipe(PythonRecipe):
+    version = '6.3.1'
+    wheel_path = '/home/shyamnath/qt_for_python/pyside-setup/dist/shiboken6-6.3.1-6.3.1-cp36-abi3-android_aarch64.whl'
+
+    call_hostpython_via_targetpython = False
+    install_in_hostpython = False
+
+    def build_arch(self, arch):
+        ''' Unzip the wheel and copy into site-packages of target'''
+        info('Installing {} into site-packages'.format(self.name))
+        with zipfile.ZipFile(self.wheel_path, 'r') as zip_ref:
+            info('Unzip wheels and copy into {}'.format(self.ctx.get_python_install_dir()))
+            zip_ref.extractall(self.ctx.get_python_install_dir())
+
+recipe = ShibokenRecipe()

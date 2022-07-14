1f508bebdef4e622795474d68682d330358c90a8
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2022-07-14 16:24:06 +0200

    add change to shiboken recipe

--- pythonforandroid/recipes/shiboken6/__init__.py
+++ pythonforandroid/recipes/shiboken6/__init__.py
@@ -4,7 +4,7 @@ import zipfile
 
 class ShibokenRecipe(PythonRecipe):
     version = '6.3.1'
-    wheel_path = '/home/shyamnath/qt_for_python/pyside-setup/dist/shiboken6-6.3.1-6.3.1-cp36-abi3-android_aarch64.whl'
+    wheel_path = '/home/shyamnath/qt_for_python/pyside-setup/dist/shiboken6-6.3.1-6.3.1-cp36-abi3-android_x86_64.whl'
 
     call_hostpython_via_targetpython = False
     install_in_hostpython = False
@@ -13,7 +13,7 @@ class ShibokenRecipe(PythonRecipe):
         ''' Unzip the wheel and copy into site-packages of target'''
         info('Installing {} into site-packages'.format(self.name))
         with zipfile.ZipFile(self.wheel_path, 'r') as zip_ref:
-            info('Unzip wheels and copy into {}'.format(self.ctx.get_python_install_dir()))
-            zip_ref.extractall(self.ctx.get_python_install_dir())
+            info('Unzip wheels and copy into {}'.format(self.ctx.get_python_install_dir(arch.arch)))
+            zip_ref.extractall(self.ctx.get_python_install_dir(arch.arch))
 
 recipe = ShibokenRecipe()

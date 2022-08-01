481d21c194ec39bb526d418c56834009601c131c
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2022-08-01 14:34:29 +0200

    fix PythonUtil.java

--- pythonforandroid/bootstraps/common/build/src/main/java/org/kivy/android/PythonUtil.java
+++ pythonforandroid/bootstraps/common/build/src/main/java/org/kivy/android/PythonUtil.java
@@ -50,9 +50,9 @@ public class PythonUtil {
         addLibraryIfExists(libsList, "SDL2_mixer", libsDir);
         addLibraryIfExists(libsList, "SDL2_ttf", libsDir);
         Log.v("Shyam", "loading Qt libraries");
-        addLibraryIfExists(libsList, "Qt6*", libsDir)
+        addLibraryIfExists(libsList, "Qt6*", libsDir);
         Log.v("Shyam", "loading c++_shared library");
-        addLibraryIfExists(libsList, "c++_shared", libsDir)
+        addLibraryIfExists(libsList, "c++_shared", libsDir);
         libsList.add("python3.5m");
         libsList.add("python3.6m");
         libsList.add("python3.7m");

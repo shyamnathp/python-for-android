384f9b42a34bcb17780fb936dc488029aaff8dfe
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2022-08-01 14:00:58 +0200

    add libraries for startup

--- pythonforandroid/recipes/PySide6/__init__.py
+++ pythonforandroid/recipes/PySide6/__init__.py
@@ -1,9 +1,27 @@
-import setuptools
+import os
 from pythonforandroid.recipe import PythonRecipe, CppCompiledComponentsPythonRecipe
 from pythonforandroid.logger import info
 import zipfile
-from pythonforandroid.toolchain import shprint
+from pythonforandroid.toolchain import shutil
+from os.path import join, isabs
+import subprocess
 
+def run_process(args, initial_env=None):
+    """
+    Run process until completion and return the process exit code.
+    No output is captured.
+    """
+    command = " ".join([(" " in x and f'"{x}"' or x) for x in args])
+    info(f"In directory {os.getcwd()}:\n\tRunning command:  {command}")
+
+    if initial_env is None:
+        initial_env = os.environ
+
+    kwargs = {}
+    kwargs['env'] = initial_env
+
+    exit_code = subprocess.call(args, **kwargs)
+    return exit_code
 
 class PySideRecipe(PythonRecipe):
     version = '6.3.1'
@@ -20,5 +38,25 @@ class PySideRecipe(PythonRecipe):
         with zipfile.ZipFile(self.wheel_path, 'r') as zip_ref:
             info('Unzip wheels and copy into {}'.format(self.ctx.get_python_install_dir(arch.arch)))
             zip_ref.extractall(self.ctx.get_python_install_dir(arch.arch))
+
+        info('Copying libc++_shared.so file to be loaded on startup')
+        shutil.copyfile(join(os.path.expanduser("~"), 'libc++_shared.so'),
+                        join(self.ctx.get_libs_dir(arch.arch), 'libc++_shared.so'))
+
+        info('Copying Qt libraries to be loaded on startup')
+        shutil.copyfile(join(self.ctx.get_python_install_dir(arch.arch), 'PySide6', 'Qt', 'lib', 'libQt6Core_x86_64.so'),
+                        join(self.ctx.get_libs_dir(arch.arch), 'libQt6Core_x86_64.so'))
+
+        info('Run patchelf on the Qt binaries')
+        executable_path = join(self.ctx.get_libs_dir(arch.arch), 'libQt6Core_x86_64.so')
+        patchelf_path = shutil.which('patchelf')
+        if not isabs(patchelf_path):
+            patchelf_path = join(os.getcwd(), patchelf_path)
+            info(f"Using {self._patchelf_path} ...")
+
+        cmd = [patchelf_path, '--set-rpath', '$ORIGIN', executable_path]
+
+        if run_process(cmd) != 0:
+            raise RuntimeError(f"Error patching rpath in {executable_path}")
 
 recipe = PySideRecipe()

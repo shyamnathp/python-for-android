51d8d8ad06d4102695629b3f2b06896258936972
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2022-10-19 14:01:51 +0200

    correct system.load-2

--- pythonforandroid/bootstraps/common/build/src/main/java/org/kivy/android/PythonUtil.java
+++ pythonforandroid/bootstraps/common/build/src/main/java/org/kivy/android/PythonUtil.java
@@ -82,7 +82,7 @@ public class PythonUtil {
         for (String lib : getLibraries(libsDir)) {
             Log.v(TAG, "Loading library: " + lib);
             try {
-                if lib.startsWith("libplugins_platforms") {
+                if (lib.startsWith("libplugins_platforms")) {
                     System.load("/home/shyamnath/qt_for_python/shyam/kivy_example/.buildozer/android/platform/build-x86_64/dists/shiboken_app/libs/x86_64/libplugins_platforms_qtforandroid_x86_64.so");
                 }
                 else

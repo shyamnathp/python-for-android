bcbb27281961431794befee0bd44a5792d02058f
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2022-10-20 16:44:20 +0200

    you are

--- pythonforandroid/bootstraps/common/build/src/main/java/org/kivy/android/PythonUtil.java
+++ pythonforandroid/bootstraps/common/build/src/main/java/org/kivy/android/PythonUtil.java
@@ -82,17 +82,18 @@ public class PythonUtil {
         for (String lib : getLibraries(libsDir)) {
             Log.v(TAG, "Loading library: " + lib);
             try {
-                // if (lib.contains("plugins_platforms")) {
-                //     String platform_plugin = libsDir.getAbsolutePath() + "/libplugins_platforms_qtforandroid_x86_64.so";
-                //     Log.v(TAG, "Loading Shyam libplugins_platforms" + platform_plugin);
-                //     System.load(platform_plugin);
-                // }
-                // else
-                // {
-                //     Log.v(TAG, "Loading Shyam libplugins_platforms not load");
-                System.loadLibrary(lib);
-                if (lib.startsWith("python")) {
-                    foundPython = true;
+                if (lib.contains("plugins_platforms")) {
+                    String platform_plugin = "/data/app/~~DCB-oEEUz5ZpNrYHaXbfUA==/org.qtproject.example.addressbook-ewGJs_dDua-qQjjYyDTc0Q==/lib/x86_64/libplugins_platforms_qtforandroid_x86_64.so";
+                    Log.v(TAG, "Loading Shyam libplugins_platforms" + platform_plugin);
+                    System.load(platform_plugin);
+                }
+                else
+                {
+                    Log.v(TAG, "Loading Shyam libplugins_platforms not load");
+                    System.loadLibrary(lib);
+                    if (lib.startsWith("python")) {
+                        foundPython = true;
+                    }
                 }
             } catch(UnsatisfiedLinkError e) {
                 // If this is the last possible libpython

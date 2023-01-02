a96227d1ee73ca5d3f1b33a5fe45c31f888c259f
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2023-01-02 11:31:49 +0100

    change import path for QtAcitivity.java

--- pythonforandroid/bootstraps/qt/build/src/main/java/org/kivy/android/PythonActivity.java
+++ pythonforandroid/bootstraps/qt/build/src/main/java/org/kivy/android/PythonActivity.java
@@ -20,7 +20,7 @@ import android.os.PowerManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
 
-import org.kivy.android.QtActivity;
+import org.qtproject.qt.android.bindings.QtActivity;
 
 public class PythonActivity extends QtActivity {
 

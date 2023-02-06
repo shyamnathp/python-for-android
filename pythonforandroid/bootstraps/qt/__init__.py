cffbc1af6ada1c1d3579ead41a8bf1d0fff810f4
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2023-02-06 14:19:33 +0100

    strip Qt libraries

--- pythonforandroid/bootstraps/qt/__init__.py
+++ pythonforandroid/bootstraps/qt/__init__.py
@@ -44,9 +44,8 @@ class QtBootstrap(Bootstrap):
                 with open('blacklist.txt', 'a') as fileh:
                     fileh.write('\nsqlite3/*\nlib-dynload/_sqlite3.so\n')
 
-        # TODO: remove this comment. We want the libraries stripped.
-        # if not self.ctx.with_debug_symbols:
-        #     self.strip_libraries(arch)
+        if not self.ctx.with_debug_symbols:
+            self.strip_libraries(arch)
         self.fry_eggs(site_packages_dir)
         super().assemble_distribution()
 

8670cbeccfcf150a2b5977632993d9c955cd683f
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2023-02-06 14:18:44 +0100

    remove Android recipe dependency

--- pythonforandroid/bootstraps/qt/__init__.py
+++ pythonforandroid/bootstraps/qt/__init__.py
@@ -9,9 +9,7 @@ class QtBootstrap(Bootstrap):
 
     name = 'qt'
 
-    recipe_depends = list(
-        set(Bootstrap.recipe_depends).union({'PySide6', 'shiboken6'})
-    )
+    recipe_depends = ['python3', 'android', 'PySide6', 'shiboken6']
 
     def assemble_distribution(self):
         info_main('# Creating Android project from build and {} bootstrap'.format(
